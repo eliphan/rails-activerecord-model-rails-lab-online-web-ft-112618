70071e662f41728036bbf201590b59811e742fb8
Elise Phan <aliceconnic@gmail.com>
2019-02-05 02:34:55 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,9 +3,9 @@ class Student < ActiveRecord::Base
   def to_s
     @students = Student.all
     @students.each do |student|
-      student.first_name = first 
-      student.last_name = last 
-      "#{@first}.to_s + last.to_s"
+      student.first_name.to_s = first 
+      student.last_name.to_s = last 
+      "#{@first} #{@last}"
     end
   end
   
