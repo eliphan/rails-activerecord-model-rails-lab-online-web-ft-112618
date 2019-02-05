5b425d94b528dc924791ec39db8c720d52479e9c
Elise Phan <aliceconnic@gmail.com>
2019-02-05 02:32:43 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,10 @@
 class Student < ActiveRecord::Base 
   
   def to_s
-    "#{@student.first_name.to_s} #{@student.last_name.to_s}"
+    @students = Student.all
+    @students.each do |student|
+      student.first_name.to_s + student.last_name.to_s
+    end
   end
   
 end
